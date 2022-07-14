9684dbb71c602ce0b388cd2c31545bd482699d49
matiaskelman <88509593+matiaskelman@users.noreply.github.com>
2022-07-14 10:38:37 -0300

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './components/Formulario';
 import { ListadoCitas } from './components/ListadoCitas';
+import { React, useState } from 'react';
 function App() {
+
+const [form, setForm] = useState(
+  {
+    nPerro : "",
+    nDueño : ""
+  }
+)
+
+
   return (
     <div className="App">
     <h1>ADMINISTRADOR DE PACIENTES</h1>
@@ -11,11 +21,14 @@ function App() {
 <div className="one-half column">
 
 <h2>Crear mi Cita</h2>
-<Form/>
+<Form
+forrm = {form}
+setForm = {setForm}
+/>
 </div>
 <div className="one-half column">
 <h2>Administra tus citas</h2>
-<ListadoCitas/>
+{/* <ListadoCitas/> */}
 </div> 
     </div>
 
